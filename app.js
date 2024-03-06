f1e18071614a7bedc77ac66bd3c5c9f0c7913ce6
Mustafa Ali <mustafaaaly27@gmail.com>
2024-03-07 03:34:52 +0500

    fifth commit

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // alert("Error ! pls enter a valid password");
 
 
-alert( "Welcome To Js Land ! \n Happy Coding...");
+// alert( "Welcome To Js Land ! \n Happy Coding...");
+
+// alert( "Hey ! I can run JS thru my developers console");
+
+
